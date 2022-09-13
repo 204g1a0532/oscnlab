4a72a8de6582bf2532bff70921d6a9deaf6d7d9d
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-09-13 12:09:26 +0530

    FCFS.c
    Giri completed the program

--- oslab/fcfs.c
+++ oslab/fcfs.c
@@ -4,18 +4,17 @@
    Date of Creation: 13-09-2022
    Date of Execution: 13-09-2022
     #include<stdio.h>
-#include<conio.h>
-main()
+
+int main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
 {
 printf("\nEnter Burst Time for Process %d -- ", i);
-scanf("%d", &bt[i]);
+scanf("%d", &bt[i]);>
 }
 wt[0] = wtavg = 0;
 tat[0] = tatavg = bt[0];
@@ -32,5 +31,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+return 0;
 }
