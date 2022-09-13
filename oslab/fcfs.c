a050ae820324017c35f344cc9179a83bc4d0fbd4
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-09-13 12:18:24 +0530

    fcfs.c
    Giri completed the program

--- oslab/fcfs.c
+++ oslab/fcfs.c
@@ -1,10 +1,9 @@
- Name Of the Candidate: Girivardhan.V
+/* Name Of the Candidate: Girivardhan.V
    Roll No: 204G1A0532
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
    Date of Creation: 13-09-2022
-   Date of Execution: 13-09-2022
-    #include<stdio.h>
-
+   Date of Execution: 13-09-2022*/
+#include<stdio.h>
 int main()
 {
 int bt[20], wt[20], tat[20], i, n;
