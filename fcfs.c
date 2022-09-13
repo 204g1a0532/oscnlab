0a396ab0c032cedecaf157208c13cd0c4eeb5d22
204g1a0532 <94513570+204g1a0532@users.noreply.github.com>
2022-09-13 11:12:37 +0530

    fcfs.c
    Girivardhan.V Created fcfs.c

---
+++ fcfs.c
@@ -0,0 +1,5 @@
+Name Of the Candidate:Girivardhan.V
+   Roll No: 204G1A0532
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022L Ashok Created Sample.cL Ashok Created Sample.c
